fcc3be3d71bd68b90f1b701fe1c08c90e84df166
jammacn <jeff@jamma.cn>
2017-03-16 21:08:08 +0800

    create

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # jm-config-mq
 mq plugin for jm-config
+
+#using
+    var _onmessage = function(channel, message){
+        logger.debug('mq channel: %s, message: %s', channel, message);
+    };
+    mq.psubscribe('config.*');
+    mq.onPMessage(function(pattern, channel, message){
+        _onmessage(channel, message);
+    });
---
+++ config/index.js
@@ -0,0 +1,28 @@
+require('log4js').configure(__dirname + '/log4js.json');
+var config = {
+    development: {
+        port:20000,
+        modules: {
+            'config': {
+                module: 'jm-config'
+            },
+            'jm-config-mq': {
+                module: process.cwd() + '/lib'
+            }
+        }
+    },
+    production: {
+        port: 20000,
+        modules: {
+            '': {
+                module: process.cwd() + '/lib'
+            }
+        }
+    }
+};
+
+var env = process.env.NODE_ENV||'development';
+config = config[env]||config['development'];
+config.env = env;
+
+module.exports = config;
